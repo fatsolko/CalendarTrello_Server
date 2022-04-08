cb1d46dbd94183c234e04e4a2d8e5eb9aa37bbbf
den <den2434358@gmail.com>
2022-04-08 19:07:37 +0400

    fix string

--- server/server.py
+++ server/server.py
@@ -118,5 +118,4 @@ def redirect_mq():
 
 if __name__ == "__main__":
     app.debug = True
-    app.run(ssl_context=('/etc/letsencrypt/live/fatsolko.xyz/fullchain.pem', '/etc/letsencrypt/live/fatsolko.xyz'
-                                                                             '/privkey.pem'), host=IP, port=PORT)
+    app.run(ssl_context=('/etc/letsencrypt/live/fatsolko.xyz/fullchain.pem', '/etc/letsencrypt/live/fatsolko.xyz/privkey.pem'), host=IP, port=PORT)

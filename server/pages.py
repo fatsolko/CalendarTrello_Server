e657715f6a2e1e3f9c444c836c8c2b0cdd9d2c01
den <den2434358@gmail.com>
2022-04-23 12:57:31 +0400

    add links

--- server/pages.py
+++ server/pages.py
@@ -9,8 +9,8 @@ index = """
 
 <HR>
 
-<b>Hi this is Fatsolko!</b>
-<b>\nThis is the website for hosting the CalendarTrello application server and authorization through Google Oauth2</b>
+<b>Hi this is Fatsolko!<br>This is the website for hosting the CalendarTrello application server and authorization through Google Oauth2</b>
+
 <p><a href="https://fatsolko.xyz/privacy_policy">Privacy policy</a></p>
 <p><a href="https://fatsolko.xyz/terms">Terms</a></p>
 
